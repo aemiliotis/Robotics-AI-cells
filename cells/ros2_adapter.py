af50ec3702fb53e5e8fe837405eb95d244ea7888
aemiliotis <aemiliotis@gmail.com>
2025-07-15 21:28:20 +0300

    Create ros2_adapter.py

---
+++ cells/ros2_adapter.py
@@ -0,0 +1,12 @@
+def process(input_data):
+    # Converts between your API and ROS2 messages
+    return {
+        "ros2_header": {
+            "stamp": {"sec": int(time.time()), "nanosec": 0},
+            "frame_id": input_data.get("frame", "base_link")
+        },
+        "twist": {
+            "linear": {"x": input_data["vel_x"], "y": 0, "z": 0},
+            "angular": {"z": input_data["ang_vel"]}
+        }
+    }
