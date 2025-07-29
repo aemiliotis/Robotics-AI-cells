62a3febfb9e5863e6d6389ee8cd0a86f4d91b6c8
aemiliotis <aemiliotis@gmail.com>
2025-07-29 19:07:43 +0300

    Update precision_scaling.py

--- cells/precision_scaling.py
+++ cells/precision_scaling.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def scale_precision(arr, factor=100):
-       """Convert floats to scaled integers"""
-       return (np.array(arr) * factor).astype(np.int16)
+    """Convert floats to scaled integers"""
+    return (np.array(arr) * factor).astype(np.int16)
