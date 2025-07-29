682b3ebfae419bdc1c24835eb8ba7afb54781484
aemiliotis <aemiliotis@gmail.com>
2025-07-29 19:03:31 +0300

    Update shared_memory.py

--- cells/shared_memory.py
+++ cells/shared_memory.py
@@ -1,7 +1,18 @@
+import time
+
 _CACHE = {}
 
-   def get_cache(key):
-       return _CACHE.get(key)
+def get_cache(key):
+    cached = _CACHE.get(key)
+    if cached is None:
+        return None
+        
+    value, expiration = cached
+    if time.time() > expiration:
+        del _CACHE[key]  # Remove expired item
+        return None
+        
+    return value
 
-   def set_cache(key, value, ttl=10):
-       _CACHE[key] = (value, time.time() + ttl)
+def set_cache(key, value, ttl=10):
+    _CACHE[key] = (value, time.time() + ttl)
